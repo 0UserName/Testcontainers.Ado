ac58f25e284fb214b20cea4478d113136c00edf7
0username <net.fedorax64@gmail.com>
2026-09-25 17:28:36 +0300

    DbAssets added

---
+++ Testcontainers.Ado.NUnit/DbAsserts.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+using System.Threading.Tasks;
+
+using Testcontainers.Ado.Contracts;
+
+namespace Testcontainers.Ado.NUnit
+{
+    public static class DbAsserts
+    {
+        /// <summary>
+        /// Applies a constraint to ensure that
+        /// the number of inserted rows matches
+        /// the expected row count.
+        /// </summary>
+        public static Task ThatInsertAsync(this IDbTestRunner runner, string procedure, object value, int expectedRows)
+        {
+            return Assert.ThatAsync(() => runner.InsertValueAsync(procedure, value), Is.EqualTo(expectedRows));
+        }
+    }
+}
