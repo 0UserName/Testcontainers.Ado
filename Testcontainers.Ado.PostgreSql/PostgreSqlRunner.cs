62a2eb1f12808f0a4a0df8dde3f12c4fe1cc1776
0username <net.fedorax64@gmail.com>
2026-09-25 03:29:45 +0300

    PostgreSqlRunner added

---
+++ Testcontainers.Ado.PostgreSql/PostgreSqlRunner.cs
@@ -0,0 +1,37 @@
+﻿using Npgsql;
+
+using System.Threading.Tasks;
+
+using Testcontainers.Ado.Contracts.Abstracts;
+
+using Testcontainers.PostgreSql;
+
+namespace Testcontainers.Ado.PostgreSql
+{
+    public class PostgreSqlRunner : AbstractDbTestRunner<PostgreSqlContainer, NpgsqlParameter>
+    {
+        /// <remarks>
+        /// See: <see href="https://hub.docker.com/_/postgres#initialization-scripts">Initialization scripts</see>.
+        /// </remarks>
+        private const string ENTRY_POINT_DIR = "/docker-entrypoint-initdb.d/";
+
+        /// <summary>
+        /// Creates a new data source builder
+        /// using the given connection string.
+        /// </summary>
+        protected virtual NpgsqlDataSourceBuilder CreateDataSourceBuilder(string connectionString)
+        {
+            return new NpgsqlDataSourceBuilder(connectionString);
+        }
+
+        /// <inheritdoc/>
+        public override async Task StartAsync(string image, string schemaDir)
+        {
+            Container = new PostgreSqlBuilder(image).WithResourceMapping(schemaDir, ENTRY_POINT_DIR).Build();
+
+            await Container.StartAsync();
+
+            Source = CreateDataSourceBuilder(Container.GetConnectionString()).Build();
+        }
+    }
+}
