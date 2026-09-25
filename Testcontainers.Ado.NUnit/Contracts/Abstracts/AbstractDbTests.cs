983e2a0d792b6c06338460b5e0787664e78f051c
0username <net.fedorax64@gmail.com>
2026-09-25 16:38:36 +0300

    AbstractDbTests added

---
+++ Testcontainers.Ado.NUnit/Contracts/Abstracts/AbstractDbTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+using System.Threading.Tasks;
+
+using Testcontainers.Ado.Contracts;
+
+namespace Testcontainers.Ado.NUnit.Contracts.Abstracts
+{
+    public abstract class AbstractDbTests<TDbTestRunner>(string image, string schemaDir) where TDbTestRunner : IDbTestRunner, new()
+    {
+        private readonly TDbTestRunner _runner = new TDbTestRunner();
+
+        protected TDbTestRunner Runner
+        {
+            get => _runner;
+        }
+
+        [OneTimeSetUp]
+        protected virtual async Task StartAsync()
+        {
+            await Runner.StartAsync(image, schemaDir);
+        }
+
+        [OneTimeTearDown]
+        protected virtual async Task StopAsync()
+        {
+            await Runner.DisposeAsync();
+        }
+    }
+}
